cad688596c09848bffc4277f6a101bbb719f326d
Edvin Havic <edvinhavic@gmail.com>
2019-04-16 13:43:22 +0200

    updated rnw-app button

--- main/generated/rnw-app/src/components/Button/index.js
+++ main/generated/rnw-app/src/components/Button/index.js
@@ -1,71 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components/native/dist/styled-components.native.cjs'
-import { TouchableOpacity, Text } from 'react-native'
+import { Button as RNPButton } from 'react-native-paper'
 
-const StyledButton = styled.View`
-  background-color: ${props => props.backgroundColor || '#03A9F4'};
-  align-self: stretch;
-  border-radius: 4px;
-  padding: 12px;
-  margin-top: 8px;
-  margin-bottom: 8px;
-  margin-right: 4px;
-  margin-left: 4px;
-`
-
-const Title = styled.Text`
-  font-size: 18px;
-  align-self: center;
-  color: ${props => props.color || 'white'};
-`
-
-const ClearButton = styled(StyledButton)`
-  background-color: transparent;
-`
-
-const OutlinedButton = styled(ClearButton)`
-  border-color: ${props => props.backgroundColor || '#03A9F4'};
-  border-width: 2px;
-`
-
-const getButton = type => {
-  switch (type) {
-    case 'solid':
-      return StyledButton
-    case 'clear':
-      return ClearButton
-    case 'outline':
-      return OutlinedButton
-    default:
-      return StyledButton
-  }
-}
-
-const Button = ({ title, type, color, backgroundColor, onPress, ...rest }) => {
-  const SelectedButton = getButton(type)
-
-  return (
-    <TouchableOpacity onPress={onPress} {...rest}>
-      <SelectedButton color={color} backgroundColor={backgroundColor}>
-        <Title color={color}>{title}</Title>
-      </SelectedButton>
-    </TouchableOpacity>
-  )
-}
+const Button = ({ title, icon, type, color, contentStyle, onPress, ...rest }) => (
+  <RNPButton
+    contentStyle={{ height: 48, ...contentStyle }}
+    icon={icon}
+    mode={type}
+    color={color}
+    onPress={onPress}
+    {...rest}
+  >
+    {title}
+  </RNPButton>
+)
 
 Button.propTypes = {
   title: PropTypes.string.isRequired,
-  type: PropTypes.oneOf(['solid', 'clear', 'outline']),
+  type: PropTypes.oneOf(['text', 'outlined', 'contained']),
   color: PropTypes.string,
-  backgroundColor: PropTypes.string,
+  icon: PropTypes.string,
   onPress: PropTypes.func,
+  contentStyle: PropTypes.object,
 }
 
 Button.defaultProps = {
   type: 'solid',
   color: 'white',
-  backgroundColor: '#03A9F4',
+  contentStyle: {},
   onPress: () => {},
 }
 
